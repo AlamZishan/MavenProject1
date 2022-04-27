fb5bcf1446d6eaa24e62bb653182303c3e2f5e9a
AlamZishan <Zishan.Alam@mindtree.com>
2022-04-27 10:59:48 +0530

    adding comment in code

--- src/main/java/mindtree/OpenGoogle.java
+++ src/main/java/mindtree/OpenGoogle.java
@@ -10,6 +10,7 @@ public class OpenGoogle {
 		System.setProperty("webdriver.chrome.driver", "D:\\Software\\selenium\\browser driver\\chromedriver.exe");
 		//creating object of chromeDriver
 		WebDriver driver = new ChromeDriver();
+		//using object driver and  get method opening google.com
 		driver.get("https://www.google.com");
 		driver.getTitle();
 	}
