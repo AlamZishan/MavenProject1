838ae7fc5c5f83c487ae70380415d808cf530bda
AlamZishan <Zishan.Alam@mindtree.com>
2022-04-27 10:34:44 +0530

    commiting after modifying the code in OpenGoogle.java

--- src/main/java/mindtree/OpenGoogle.java
+++ src/main/java/mindtree/OpenGoogle.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class OpenGoogle {
 
 	public static void main(String[] args) {
-		//setting path of webdriver hey
+		//setting path of chrome webdriver.exe file
 		System.setProperty("webdriver.chrome.driver", "D:\\Software\\selenium\\browser driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
